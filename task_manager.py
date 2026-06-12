d447b16c757dcf5074b3eaaaec58ade176c210e6
Michael Denver Jumah <michaeljumah8@gmail.com>
2026-06-12 16:28:08 +0300

    creating a linked list

---
+++ task_manager.py
@@ -0,0 +1,11 @@
+class Node:
+    def def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def def __init__(self):
+        self.head = None
+
+    
+
