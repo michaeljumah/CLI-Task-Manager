4f9e9bb73d02bdf14e45d851f8cffc6c2f3eb9a4
Michael Denver Jumah <michaeljumah8@gmail.com>
2026-06-18 16:30:18 +0300

    self.head initialization

--- task_manager.py
+++ task_manager.py
@@ -8,5 +8,11 @@ class LinkedList:
     def def __init__(self):
         self.head = None
 
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+
     
 
