5763a9030c3103dfb9ace0769a26bd43601687ac
Michael Denver Jumah <michaeljumah8@gmail.com>
2026-06-19 19:25:24 +0300

    delete feature added

--- task_manager.py
+++ task_manager.py
@@ -1,11 +1,11 @@
 class Node:
-    def def __init__(self, data):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
 #comment
 class LinkedList:
-    def def __init__(self):
+    def __init__(self):
         self.head = None
 
     def append(self, data):
@@ -14,6 +14,28 @@ class LinkedList:
             self.head = new_node
             return
         current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    
+    def delete(self, task_name):
+        if not self.head:
+            return None
+        if self.head.data["name"] == task_name:
+            removed = self.head.data
+            self.head = self.head.next
+            return removed
+        current = self.head
+        while current.next:
+            if current.next.data["name"] == task_name:
+                removed = current.next.data
+                current.next = current.next.next
+                return removed
+            current = current.next
+        return None
+
+
 
     
 
